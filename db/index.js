aa5186ddf7a8902e32f24701ae8c144ac12ff817
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2022-08-21 11:27:05 +0200

    DB seeded

--- db/index.js
+++ db/index.js
@@ -1,12 +1,4 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
-// Import of the data from './data.json'
-const data = require('./data');
-
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/food-wars";
 
 mongoose
