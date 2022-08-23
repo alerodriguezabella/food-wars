48953efaac9fc23528e2e558a364d4749ed045e3
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2022-08-23 15:37:00 +0200

    added images via cloudinary in the create and edit recipe hbs

--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Recipe = require("../models/Recipe.model");
 const isAdmin = require("../middleware/isAdmin");
+const fileUploader = require('../config/cloudinary.config');
 
 router.get('/recipe-list', (req,res) => {
     const user = req.session.user;
@@ -39,10 +40,14 @@ router.get('/new-recipe', isAdmin, (req,res) => {
     // .catch( Err => console.error(Err));
 })
 
-router.post('/new-recipe', isAdmin, (req,res) => {
+router.post('/new-recipe', isAdmin, fileUploader.single('image'), (req,res) => {
+    let imageDef = req.file === undefined ? 'https://images.unsplash.com/photo-1628209694088-9aa9ac1c6463?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80' : req.file.path  
     const {title, level, ingredients, instructions, dishType, image, duration} = req.body;
-    Recipe.create({title, level, ingredients: ingredients.split(';'), instructions: instructions.split(';'), dishType, image, duration})
-    .then(() => res.redirect('/recipes/recipe-list'))
+    Recipe.create({title, level, ingredients: ingredients.split(';'), instructions: instructions.split(';'), dishType, image: imageDef, duration})
+    .then(() => {
+        res.redirect('/recipes/recipe-list')
+        // console.log(req.file)
+    })
     .catch(Err => console.error(Err))
 })
 
@@ -52,9 +57,16 @@ router.get('/edit-recipe/:id', isAdmin, (req,res) => {
     .catch(Err => console.error(Err))
 })
 
-router.post('/edit-recipe/:id', isAdmin, (req,res) => {
-    const {title, level, ingredients, instructions, dishType, image, duration} = req.body;
-    Recipe.findByIdAndUpdate(req.params.id, {title, level, ingredients: ingredients.split(';'), instructions: instructions.split(';'), dishType, image, duration})
+router.post('/edit-recipe/:id', isAdmin, fileUploader.single('image'), (req,res) => {
+    const {title, level, ingredients, instructions, dishType, image, duration, existingImage} = req.body;
+    let imageDef;
+    if(req.file){
+        imageDef = req.file.path
+    } else {
+        imageDef = existingImage
+    }
+
+    Recipe.findByIdAndUpdate(req.params.id, {title, level, ingredients: ingredients.split(';'), instructions: instructions.split(';'), dishType, image: imageDef, duration})
     .then(() => res.redirect('/recipes/recipe-list'))
     .catch(Err => console.error(Err))
 })
