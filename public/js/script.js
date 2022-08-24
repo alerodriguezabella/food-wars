5be2dd10d21cd0e4ff4d34f856335dd13a63a1fc
Eurico Moutinho <108530777+eurico-moutinho@users.noreply.github.com>
2022-08-24 18:48:21 +0100

    changes on style

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,12 @@ document.addEventListener(
 );
 
 
+  let rank =document.getElementById('admin').textContent;
+  console.log(rank)
+  if(rank=='true'){
+    document.getElementById('admin').textContent='Super User';
+  }else if(rank==='false'){
+    document.getElementById('admin').textContent='Peasant';
+  }
+
+
