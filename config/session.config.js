b5ba8b6c78534d218567d88ab65d98f749273ac4
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2022-08-25 12:12:24 +0200

    changes dep

--- config/session.config.js
+++ config/session.config.js
@@ -24,11 +24,10 @@ module.exports = app => {
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 // 60 * 1000 ms === 1 min
       },
-      store: new MongoStore({
-        mongoUrl: 'mongodb://localhost/food-wars',
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
         ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day      
       })
     })
   );
 };
-
